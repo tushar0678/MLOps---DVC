37f25cadd18735593fa993b6e929c3c3f1d823b4
Tushar Shukla <tusharshukla211@gmail.com>
2025-08-01 13:11:37 +0530

    second / third version of data changed

--- dvc-code.py
+++ dvc-code.py
@@ -2,19 +2,19 @@ import pandas as pd
 import os
 
 # Create a sample DataFrame with column names
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
+data = {'Name': ['Ramesh', 'Mohit', 'Ashish'],
     'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
+    'City': ['Agra', 'Noida', 'Mathura']
     }
 
 df = pd.DataFrame(data)
 
 # # Adding new row to df for V2
-new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+new_row_loc = {'Name': 'Rani', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
 # # Adding new row to df for V3
-new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+new_row_loc2 = {'Name': 'Rekha', 'Age': 30, 'City': 'City2'}
 df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at the root level
