188a2ad9db4542f9bd26a09598df5bdf3e209982
Tushar Shukla <tusharshukla211@gmail.com>
2025-08-01 13:14:49 +0530

    fourth version of data changed

--- dvc-code.py
+++ dvc-code.py
@@ -17,6 +17,9 @@ df.loc[len(df.index)] = new_row_loc
 new_row_loc2 = {'Name': 'Rekha', 'Age': 30, 'City': 'City2'}
 df.loc[len(df.index)] = new_row_loc2
 
+new_row_loc3 = {'Name': 'Madhu', 'Age': 28, 'City': 'Firozabad'}
+df.loc[len(df.index)] = new_row_loc3
+
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
